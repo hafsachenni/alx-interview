4b278c1e49a55588e6d27483aee644dadb32f297
hafsa chenni <hafsachenni77@gmail.com>
2024-07-09 11:22:31 +0200

    all characters of star wars movie printed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const reqst = require('request');
 const util = require('util');
 const promise = util.promisify(reqst);
 
-const getCharacter = async (url) => {
+const getCharacters = async (url) => {
   const rspn = await promise(url);
   if (rspn.statusCode === 200) console.log(JSON.parse(rspn.body).name);
 };
@@ -15,7 +15,7 @@ const getMovies = async (id) => {
   if (rspn.statusCode === 200) {
     const characters = JSON.parse(rspn.body).characters;
     for (const character of characters) {
-      await getCharacter(character);
+      await getCharacters(character);
     }
   }
 };
